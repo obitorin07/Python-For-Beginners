3f00af2bf9c475b5d4262a7564856512d8df6c27
Obito <tommylover86@gmail.com>
2024-02-02 13:53:01 +0530

    Swap Two Number Using User Input

---
+++ swap_two_no.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+no1 = float(input("Enter Number First Number:"))
+no2 = float(input("Enter Number Second Number:"))
+
+no1,no2 =no1,no2
+print("Before swap Numbers :{} {}".format(no1,no2))
+no1,no2 =no2,no1
+
+print("After swap Numbers :{} {}".format(no1,no2))
+
+

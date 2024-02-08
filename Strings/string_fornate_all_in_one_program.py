ec36d7227e884953d48cf69cac4f01aedc8eb4f6
Obito <tommylover86@gmail.com>
2024-02-08 17:54:40 +0530

    New str file

--- Strings/string_fornate_all_in_one_program.py
+++ Strings/string_fornate_all_in_one_program.py
@@ -2,3 +2,8 @@
 
 user = input("Enter Anything:")
 
+#these are important
+print("This Is for uppercase :",user.upper())
+print("This Is for Lowercase :",user.lower())
+print("This Is for titlecase :",user.title())
+print("This Is for Capitalize :",user.capitalize())

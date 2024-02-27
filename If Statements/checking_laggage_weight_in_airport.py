5ebbcf6bbdbc07d473e13f376bc8809e171eddc8
Obito <tommylover86@gmail.com>
2024-02-27 14:05:30 +0530

    note :modified variable for readability Enhance user input validation and airline-specific luggage checks

--- If Statements/checking_laggage_weight_in_airport.py
+++ If Statements/checking_laggage_weight_in_airport.py
@@ -24,18 +24,18 @@
     
 airline =input("Choose Airline from below menu :\n1.AirIndia\n2.Emirates {hint : just type (1/2)}:")
 
-AI_weight_limit =30
-EM_weight_limit=35
+air_india_weight_limit =30
+emirates_weight_limit=35
 
 if airline=='1' or airline =='2':
     luggage_weight =int(input("Enter The laggage Weight: "))
     if airline=='1':
-        if luggage_weight<=AI_weight_limit:
+        if luggage_weight<=air_india_weight_limit:
             print("checkin is cleared")
         else:
             print("Remove some luggage and comeback")
     elif airline=='2':
-        if EM_weight_limit>=luggage_weight:
+        if emirates_weight_limit >=luggage_weight:
             print("checkin in cleared")
             
         else:

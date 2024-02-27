109c47c3f189f7daabfe33932b110dd525095e57
Obito <tommylover86@gmail.com>
2024-02-27 13:23:46 +0530

    this is basic counter program

---
+++ While-Loop/counter_program.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# =============================================================================
+# Q1 of 1
+
+# What is the outcome of the following pseudo-code?
+
+# input Counter
+# while(Counter<5) do
+#   Counter=Counter+1
+#   display Counter
+# end-while
+
+# Assume that the input value provided to variable, Counter is 1.
+# =============================================================================
+counter =1
+while (counter <5):
+    counter =counter+1
+    print(counter)
+    
+    # first iteration will be 1+1 2
+    #second iteraration 2+1 3
+    # so go on  until condition is 5 
+    #output will be 2,3,4,5

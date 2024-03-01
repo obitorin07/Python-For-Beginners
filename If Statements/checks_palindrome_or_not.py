4a0f4eab330f801804daa2cc602d345803f75412
Obito <tommylover86@gmail.com>
2024-03-01 15:26:41 +0530

    Added basic Open And Read Files Code

--- If Statements/checks_palindrome_or_not.py
+++ If Statements/checks_palindrome_or_not.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-user_input = input("Enter The string you want to check is palindrome or not :").lower()
-
-if user_input == user_input[::-1]:
-    
-    print(f"{user_input} Is palindrome")
-else:
-    print(f"{user_input} Not palindrome")
+import random
+x = random.random('head','tails')
+print(x)

25a40581f0ffdff7d054726cab9f109de78e15c2
Obito <tommylover86@gmail.com>
2024-03-01 20:16:04 +0530

    file removed

--- If Statements/checks_palindrome_or_not.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-user_input = input("Enter The string you want to check is palindrome or not :").lower()
-
-if user_input == user_input[::-1]:
-    
-    print(f"{user_input} Is palindrome")
-else:
-    print(f"{user_input} Not palindrome")

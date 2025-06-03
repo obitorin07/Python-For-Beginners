2de67f229b1a07b23a8107ebda48a18662b9b420
Kiran Data Analyst <157777144+obitorin07@users.noreply.github.com>
2025-06-03 14:23:50 +0530

    new

---
+++ list/first.py
@@ -0,0 +1,20 @@
+"""
+✅ LEVEL 0 – LIST FUNDAMENTALS
+🧠 Problem 1: Product List Checker
+You run a small Kirana store.
+Your tasks:
+Print the first and last item.
+
+Print how many items are in the list.
+
+Check if "butter" is in the list.
+
+Check if "salt" is in the list
+
+
+Add "salt" to the list and print the updated list.
+"""
+
+products = ["bread", "milk", "eggs", "butter", "rice"]
+print(f"First Item :{products[0]} and last Item is : {products[-1]}")
+

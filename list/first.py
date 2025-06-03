f55d42aa10756ed948d844862ca032d3818b95aa
Kiran Data Analyst <157777144+obitorin07@users.noreply.github.com>
2025-06-03 14:27:16 +0530

    Update first.py

--- list/first.py
+++ list/first.py
@@ -16,5 +16,18 @@ Add "salt" to the list and print the updated list.
 """
 
 products = ["bread", "milk", "eggs", "butter", "rice"]
+
 print(f"First Item :{products[0]} and last Item is : {products[-1]}")
 
+print("The Number of items in store", len(products))
+
+if "butter" in products :
+    print("yes butter available")
+else:
+    print("sorry bro we dont have")
+
+user = "salt"
+if user.strip().lower() in products :
+    print(f"{user} is available do u want ?")
+else :
+    print(f"{user} not available")

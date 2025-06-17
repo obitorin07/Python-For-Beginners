178fb634828a57c934d69e737442ea009e8e8cdd
Kiran Data Analyst <157777144+obitorin07@users.noreply.github.com>
2025-06-17 14:55:15 +0530

    While loop fundamentals practice

---
+++ While-Loop/problem3.py
@@ -0,0 +1,24 @@
+## 🎯 Challenge #3: ATM PIN System 🏦
+
+atm_pin =  1234
+user_id = 'kira'
+balance = 2500
+attempt = 0
+while True :
+    user_name = input("Enter The User Id :").strip().lower()
+    if user_name == user_id :
+        while attempt < 3:
+            atm_pass = input("Enter Your ATM PIN :").strip()
+            if not atm_pass.isdigit():
+                print("Please Enter Digits")
+            if atm_pass ==atm_pin:
+                print(f"Welcome {user_id}! Your current balance is {balance}.")
+                exit()
+            attempt += 1
+            print(f"Incorrect PIN Attempt Left : {3 - attempt}")
+        print('Too many incorrect attempts Try again some time later')
+        break
+
+    else:
+        print('Please enter valid user id')
+
